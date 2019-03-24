633fb18668bbb3a27096729adb09ec8c3746b2e8
Wan Cheul Kim <wckim88@gmail.com>
2019-03-24 04:07:30 -0700

    basic stock panel functionality setup

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,3 +1,25 @@
 <template>
-  <h1>The Stocks Component</h1>
+  <div>
+    <app-stock v-for="stock in stocks" :stock="stock"></app-stock>
+  </div>
 </template>
+
+<script>
+  import Stock from './Stock.vue';
+
+  export default {
+    data() {
+      return {
+        stocks: [
+          { id: 1, name: 'dummy_1', price: 100 },
+          { id: 2, name: 'dummy_2', price: 100 },
+          { id: 3, name: 'dummy_3', price: 100 },
+          { id: 4, name: 'dummy_4', price: 100 }
+        ]
+      }
+    },
+    components: {
+      appStock: Stock
+    }
+  }
+</script>
