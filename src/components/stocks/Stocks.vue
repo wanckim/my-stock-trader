4f64da09782bbc150b715200d29d4e3cc673b9a0
Wan Cheul Kim <wckim88@gmail.com>
2019-03-25 10:26:31 -0700

    stocks setup to managed through central vuex store

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -8,18 +8,13 @@
   import Stock from './Stock.vue';
 
   export default {
-    data() {
-      return {
-        stocks: [
-          { id: 1, name: 'dummy_1', price: 100 },
-          { id: 2, name: 'dummy_2', price: 100 },
-          { id: 3, name: 'dummy_3', price: 100 },
-          { id: 4, name: 'dummy_4', price: 100 }
-        ]
-      }
-    },
     components: {
       appStock: Stock
+    },
+    computed: {
+      stocks() {
+        return this.$store.getters.stocks;
+      }
     }
   }
 </script>
