418007be83cdee2c499d89520e3d0386b0552a6a
Wan Cheul Kim <wckim88@gmail.com>
2019-03-26 08:48:27 -0700

    vue-resource and firebase setup

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
+
 import App from './App.vue'
 import { routes } from './routes'
 import store from './store/store'
 
 Vue.use(VueRouter);
+Vue.use(VueResource);
+
+Vue.http.options.root = 'https://vuejs-my-stock-trader-3c8f9.firebaseio.com/';
 
 Vue.filter('currency', (value) => {
   return '$' + value.toLocaleString();
